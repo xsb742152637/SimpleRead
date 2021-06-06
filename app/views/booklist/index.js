52e636b11165c5cc946326ccb9447b0b44243684
xie <742152637@qq.com>
2021-06-06 19:33:18 +0800

    调整

--- app/views/booklist/index.js
+++ app/views/booklist/index.js
@@ -10,7 +10,8 @@ import {
   View,
   FlatList,
   TouchableOpacity,
-  Image,
+  Modal,
+  Dimensions,
   BackHandler,
 } from 'react-native';
 // 公共样式参数
@@ -24,12 +25,14 @@ import {
   cloneObj,
 } from '@/utils/function';
 import Item from '@/components/item';
+const {width, height} = Dimensions.get('screen'); // 整个显示屏幕的宽高，包括顶部的状态信息栏
 
 export default class BookList extends React.Component {
   // 构造函数，可以在里面初始化props和state
   constructor(props) {
     super(props);
     this.state = {
+      isSetting: false,
       lastBackPressed: null,
       callbackKey: 'bookList',
       bookList: [],
@@ -119,33 +122,111 @@ export default class BookList extends React.Component {
       />
     );
   }
+
+  _showSetting() {
+    this.setState({isSetting: true});
+  }
+  renderHeader() {
+    return (
+      <View style={global.appStyles.header}>
+        <View>
+          <Text style={global.appStyles.headerText}>简 阅</Text>
+        </View>
+        <View style={styles.tools}>
+          {/*<TouchableOpacity*/}
+          {/*  style={styles.myButton}*/}
+          {/*  onPress={() => this._goReadHistory()}>*/}
+          {/*  <MyIcon*/}
+          {/*    name={'yuedujilu'}*/}
+          {/*    style={global.appStyles.headerIcon}*/}
+          {/*    size={StyleConfig.fontSize.icon}*/}
+          {/*  />*/}
+          {/*</TouchableOpacity>*/}
+          <TouchableOpacity
+            style={styles.myButton}
+            onPress={() => this._showSetting()}>
+            <MyIcon
+              name={'shezhi'}
+              style={global.appStyles.headerIcon}
+              size={StyleConfig.fontSize.icon}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => this._goSearch()}>
+            <MyIcon
+              name={'sousuo'}
+              style={global.appStyles.headerIcon}
+              size={StyleConfig.fontSize.icon}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+  _onDismiss() {
+    console.log('点到了');
+    this.setState({isSetting: false});
+  }
+  renderSetting() {
+    return (
+      <Modal
+        animationType="fade" // 淡入淡出
+        transparent={true} // 背景透明
+        visible={this.state.isSetting} // 是否显示
+        onDismiss={this._onDismiss.bind(this)}
+        onRequestClose={this._onDismiss.bind(this)}>
+        <TouchableOpacity
+          style={{
+            width: width,
+            height: height,
+          }}
+          activeOpacity={1}
+          onPress={() => {
+            this._onDismiss.bind(this);
+          }}
+        />
+        <View
+          style={[
+            global.appStyles.padding,
+            {
+              backgroundColor: '#fff',
+              width: 140,
+              position: 'absolute',
+              top: 45,
+              right: 10,
+              borderRadius: StyleConfig.radius.button,
+            },
+          ]}>
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'flex-start',
+            }}>
+            <MyIcon
+              name={'shuaxin'}
+              style={{
+                width: 30,
+                color: StyleConfig.color.text,
+              }}
+              size={StyleConfig.fontSize.icon}
+            />
+            <Text
+              style={{
+                color: StyleConfig.color.text,
+              }}>
+              更新连载书
+            </Text>
+          </View>
+        </View>
+      </Modal>
+    );
+  }
   render() {
     let isEmpty = this.state.bookList.length == 0;
     return (
       <View style={global.appStyles.content}>
-        <View style={global.appStyles.header}>
-          <View>
-            <Text style={global.appStyles.headerText}>简 阅</Text>
-          </View>
-          <View style={styles.tools}>
-            <TouchableOpacity
-              style={styles.myButton}
-              onPress={() => this._goReadHistory()}>
-              <MyIcon
-                name={'yuedujilu'}
-                style={global.appStyles.headerIcon}
-                size={StyleConfig.fontSize.icon}
-              />
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => this._goSearch()}>
-              <MyIcon
-                name={'sousuo'}
-                style={global.appStyles.headerIcon}
-                size={StyleConfig.fontSize.icon}
-              />
-            </TouchableOpacity>
-          </View>
-        </View>
+        {this.renderHeader()}
+        {this.renderSetting()}
         {isEmpty ? (
           <View style={styles.emptyView}>
             <Text style={styles.emptyText}>不要试图揣摩读书的快乐</Text>
